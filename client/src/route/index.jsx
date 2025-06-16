139cc57c04a8fb2fce6e72d5b2376a411bce55ae
prem patel <prem6666patel@gmail.com>
2025-06-16 14:41:20 +0530

    Add upload  page

--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -14,7 +14,7 @@ import MyOrders from "../pages/MyOrders";
 import Address from "../pages/Address";
 import Category from "../pages/Category";
 import SubCategory from "../pages/SubCategory";
-import UploadProduct from "../pages/uploadProduct";
+import UploadProduct from "../pages/UploadProduct";
 import Products from "../pages/Products";
 import ProductsAdmin from "../pages/ProductsAdmin";
 import AdminPermission from "../layout/AdminPermission";
